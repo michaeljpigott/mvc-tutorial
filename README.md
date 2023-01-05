698b87a4d187602339010632d0e98d963495c882
michaeljpigott <35479648+michaeljpigott@users.noreply.github.com>
2023-01-05 13:35:02 +1100

    Created controller and routes

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # mvc-tutorial
+
+# Tutorial can be found at this link: https://blog.logrocket.com/building-structuring-node-js-mvc-application/
---
+++ index.js
@@ -0,0 +1,8 @@
+//js
+const express = require("express");
+const app = express();
+app.set("view engine", "ejs");
+//Routes
+app.use("/", require("./routes/login"));
+const PORT = process.env.PORT || 4111;
+app.listen(PORT, console.log("Server has started at port " + PORT));
